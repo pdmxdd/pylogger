9efe1ca56eb65be5099bf6009d52ddc5a617894c
Paul Matthews <paul@paulmatthews.dev>
2022-03-21 22:40:53 -0500

    remove on_release

--- pylogger/main.py
+++ pylogger/main.py
@@ -7,14 +7,8 @@ def on_press(key):
     except AttributeError:
         print('special key {0} pressed'.format(key))
 
-def on_release(key):
-    print('{0} released'.format(key))
-    if key == keyboard.Key.esc:
-        # Stop listener
-        return False
-
 if __name__ == "__main__":
-    listener = keyboard.Listener(on_press=on_press, on_release=on_release)
+    listener = keyboard.Listener(on_press=on_press)
     listener.start()
     sleep(30)
     listener.stop()
